f3ee7b6cf745c57601e337e0f14201161e099182
Joren-vanGoethem <jorenvangoethem@hotmail.com>
2023-12-16 17:56:56 +0100

    update importer to make use of new Frost Api for testing endpoints

--- Importer/Importers/EndpointImporter.cs
+++ Importer/Importers/EndpointImporter.cs
@@ -3,6 +3,7 @@ using System.Net;
 using System.Text;
 using DKSRDomain;
 using Importer.Constants;
+using Microsoft.Extensions.Logging;
 
 namespace Importer.Importers;
 
@@ -11,31 +12,69 @@ public class EndpointImporter
     private HttpClient? _client;
     public static string Username { get; set; }
     public static string Password { get; set; }
+    public readonly ILogger _logger;
     
-    public EndpointImporter(string username, string password)
+    public EndpointImporter(ILogger logger, string username, string password)
     {
         Username = username;
         Password = password;
+        _logger = logger;
     }
     
     public async Task Start()
     {
+        await SetupHttpClient();
         await StartTreesenseImporter();
         await StartParkingLotImporter();
     }
-    
-    
-    private async Task StartParkingLotImporter()
+
+    private async Task SetupHttpClient()
     {
-        // create a thread that calls the GetParkingLotSensorData method every minute
-        var importerTimer = new Timer(async _ => await GetParkingLotSensorData(), null, 0, 60 * 1000);
+        if (_client == null)
+        {
+            HttpClientHandler handler = new HttpClientHandler()
+            {
+                AutomaticDecompression = DecompressionMethods.All
+            };        
+            _client = new HttpClient(handler);
+        }
+
+        if (!_client.DefaultRequestHeaders.Contains("Accept"))
+        {
+            _client.DefaultRequestHeaders.Add("Accept", "application/json");
+        }
         
-        importerTimer?.Dispose();
+        if (!_client.DefaultRequestHeaders.Contains("User-Agent"))
+        {
+            _client.DefaultRequestHeaders.Add("User-Agent",  "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36");
+        }
+        
+        if (!_client.DefaultRequestHeaders.Contains("Accept-Encoding"))
+        {
+            _client.DefaultRequestHeaders.Add("Accept-Encoding", "gzip, deflate, br");
+        }
+        
+        if (!_client.DefaultRequestHeaders.Contains("Accept-Language"))
+        {
+            _client.DefaultRequestHeaders.Add("Accept-Language", "en-US,en;q=0.9,nl;q=0.8");
+        }
+        
+        if (!_client.DefaultRequestHeaders.Contains("Authorization"))
+        {
+            _client.DefaultRequestHeaders.Add("Authorization", $"Basic {Convert.ToBase64String(Encoding.UTF8.GetBytes($"{Username}:{Password}"))}");
+        }
     }
     
     private async Task StartTreesenseImporter()
     {
-        
+        _logger.LogInformation("Starting TreeSense Sensor Data Collection");
+        var importerTimer = new Timer(async _ => await GetTreesenseSensorData(), null, 0, 60 * 1000 * 60);
+    }
+    
+    private async Task StartParkingLotImporter()
+    {
+        _logger.LogInformation("Starting Parking Lot Sensor Data Collection");
+        var importerTimer = new Timer(async _ => await GetParkingLotSensorData(), null, 0, 60 * 1000);
     }
     
     private async Task<TreesenseResponse> GetTreesenseSensorData()
@@ -56,26 +95,6 @@ public class EndpointImporter
     
     private HttpRequestMessage CreateRequest(string path)
     {
-        if (_client == null)
-        {
-            HttpClientHandler handler = new HttpClientHandler()
-            {
-                AutomaticDecompression = DecompressionMethods.All
-            };        
-            _client = new HttpClient(handler);
-        }
-
-        if (!_client.DefaultRequestHeaders.Contains("Accept"))
-        {
-            _client.DefaultRequestHeaders.Add("Accept", "application/json");
-        }
-        
-        if (!_client.DefaultRequestHeaders.Contains("Authorization"))
-        {
-            _client.DefaultRequestHeaders.Add("Authorization", $"Basic {Convert.ToBase64String(Encoding.UTF8.GetBytes($"{Username}:{Password}"))}");
-
-        }
-        
         return new HttpRequestMessage {
             RequestUri = new Uri(path),
             Version = new Version(2, 0) // has to be 1.1 for some older api's, here 2.0 worked 
@@ -89,27 +108,23 @@ public class EndpointImporter
 
         try
         {
-            Console.WriteLine("sending request");
+            _logger.LogDebug("sending request");
             HttpResponseMessage response = await _client!.SendAsync(request);
-            Console.WriteLine("received response");
+            _logger.LogDebug("received response");
             if (response.IsSuccessStatusCode)
             {
-                Console.WriteLine("Processing response");
-                var content = response.Content.ReadAsByteArrayAsync().Result;
-                string stringencoding = System.Text.Encoding.UTF8.GetString(content, 0, content.Length);
-                var json = Newtonsoft.Json.JsonConvert.DeserializeObject<T>(stringencoding);
-                Console.WriteLine(json);
+                _logger.LogDebug("Processing response");
                 result = await processResponse(response);
-                Console.WriteLine("Processed response");
+                _logger.LogDebug("Processed response");
             }
             else
             {
-                Console.WriteLine(response.StatusCode);
+                _logger.LogWarning(response.StatusCode.ToString());
             }
         }
-        catch
+        catch (Exception e)
         {
-            Console.WriteLine("Error, rate limit perhaps");
+            _logger.LogError(e.Message);
         }
 
         return result;
--- Importer/Program.cs
+++ Importer/Program.cs
@@ -1,8 +1,11 @@
-﻿using System.Net.WebSockets;
-using System.Text;
-using Importer.Constants;
+﻿
+using System.Diagnostics;
+using FrostApi.ResponseModels;
 using Importer.Importers;
 using Microsoft.Extensions.Configuration;
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace Importer;
 
@@ -10,37 +13,64 @@ class Program
 {
     private static string _username = "";
     private static string _password = "";
-    private static string _token = "";
-    private static List<ClientWebSocket> _webSockets = new List<ClientWebSocket>();
-
+    private readonly ILogger<Program> _logger;
 
     static async Task<int> Main(string[] args)
     {
         var builder = new ConfigurationBuilder()
             .SetBasePath(Directory.GetCurrentDirectory())
             .AddJsonFile("appsettings.json", optional: false);
-
+        
         IConfiguration config = builder.Build();
-
+        
+        var services = new ServiceCollection();
+        ConfigureServices(services);
+        IServiceProvider serviceProvider = services.BuildServiceProvider();
+        var _logger = serviceProvider.GetRequiredService<ILogger<Program>>();
+        
         var authentication = config.GetSection("Authentication");
-
+        
         if (!authentication.Exists())
             throw new Exception("Authentication section not found in appsettings.json");
-
+        
         _username = authentication["Username"];
         _password = authentication["Password"];
-
-        Console.WriteLine($"{_username}, {_password}");
-
+        
+        _logger.LogInformation($"{_username}, {_password}");
+        
         if (string.IsNullOrWhiteSpace(_username) || string.IsNullOrWhiteSpace(_password))
         {
             throw new Exception("Username or password is empty");
         }
-
-        var importer = new EndpointImporter(_username, _password);
-        await importer.Start();
+        
+        var importer = new EndpointImporter(_logger, _username, _password);
+        // await importer.Start();
+        
+        await TestFrost(config["FrostBaseUrl"]);
+        
+        Process.GetCurrentProcess().WaitForExit();
         
         return 1;
+    }
+    
+    static void ConfigureServices(IServiceCollection services)
+    {
+        services
+            .AddLogging(builder => builder.AddConsole())
+            .Configure<LoggerFilterOptions>(cfg => cfg.MinLevel = LogLevel.Information);
+    }
+    
+    static async Task TestFrost(string baseUrl)
+    {
+        var api = new FrostApi.FrostApi(baseUrl);
+        var things = api.Things;
+        // var thing = await things.GetAllThings();
+        await things.PostThing(new Thing()
+        {
+            Name = "BaumTest",
+            Description = "TreeSense tree test"
+        });
+        // Console.WriteLine(thing.Value[1].Name);
     }
 
 }
