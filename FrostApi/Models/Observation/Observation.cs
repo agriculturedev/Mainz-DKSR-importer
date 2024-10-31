8e74ae9edded096bbd2e848ba9792cdba671c35e
Joren-vanGoethem <jorenvangoethem@hotmail.com>
2024-10-31 17:33:15 +0100

    update frost model

--- FrostApi/Models/Observation/Observation.cs
+++ FrostApi/Models/Observation/Observation.cs
@@ -5,13 +5,16 @@ namespace FrostApi.Models.Observation;
 public class Observation
 {
     [JsonProperty("id")] public int Id { get; set; }
-
     [JsonProperty("result")] public dynamic Result { get; set; } = null!;
-
     [JsonProperty("phenomenonTime")] public DateTime PhenomenonTime { get; set; }
     [JsonProperty("resultTime")] public DateTime ResultTime { get; set; }
-
     [JsonProperty("Datastream")] public Dictionary<string, string> DataStream { get; set; } = null!;
 
     [JsonProperty("FeatureOfInterest")] [JsonIgnore] public Dictionary<string, string> FeatureOfInterest { get; set; } = null!;
+
+    [JsonIgnore] public string Name { get; set; } = null!;
+    [JsonIgnore] public ObservedProperty.ObservedProperty ObservationType { get; set; } = ObservedProperty.ObservedProperty.Unknown();
+    [JsonIgnore] public string UnitOfMeasurementDefinition { get; set; } = String.Empty;
+    [JsonIgnore] public string UnitOfMeasurementName { get; set; } = String.Empty;
+    [JsonIgnore] public string UnitOfMeasurementSymbol { get; set; } = String.Empty;
 }
--- FrostApi/Models/ObservedProperty/ObservedProperty.cs
+++ FrostApi/Models/ObservedProperty/ObservedProperty.cs
@@ -13,4 +13,104 @@ public class ObservedProperty
     [JsonProperty("definition")] public string Definition { get; set; } = null!;
 
     [JsonProperty("properties")] public Dictionary<string, string> Properties { get; set; } = null!;
+
+    public static ObservedProperty Unknown()
+    {
+        return new ObservedProperty
+        {
+            Name = "Unknown",
+            Description = "Unknown observed property",
+            Definition = "Unknown",
+        };
+    }
+
+    public static ObservedProperty HealthState()
+    {
+        return new ObservedProperty
+        {
+            Name = "HealthState",
+            Description = "HealthState of a tree",
+            Definition = "HealthState",
+        };
+    }
+
+    public static ObservedProperty Occupancy()
+    {
+        return new ObservedProperty
+        {
+            Name = "Occupancy",
+            Description = "Occupancy of a parking space",
+            Definition = "Occupancy",
+        };
+    }
+
+    public static ObservedProperty Temperature()
+    {
+        return new ObservedProperty
+        {
+            Name = "Temperature",
+            Description = "Temperature of an observation",
+            Definition = "Temperature",
+        };
+    }
+
+    public static ObservedProperty Pressure()
+    {
+        return new ObservedProperty
+        {
+            Name = "Pressure",
+            Description = "Pressure of an observation",
+            Definition = "Pressure",
+        };
+    }
+
+    public static ObservedProperty Speed()
+    {
+        return new ObservedProperty
+        {
+            Name = "Speed",
+            Description = "Speed of an observation",
+            Definition = "Speed",
+        };
+    }
+
+    public static ObservedProperty Humidity()
+    {
+        return new ObservedProperty
+        {
+            Name = "Humidity",
+            Description = "Humidity of an observation",
+            Definition = "Humidity",
+        };
+    }
+
+    public static ObservedProperty Timestamp()
+    {
+        return new ObservedProperty
+        {
+            Name = "Timestamp",
+            Description = "Timestamp of an observation",
+            Definition = "Timestamp",
+        };
+    }
+
+    public static ObservedProperty Direction()
+    {
+        return new ObservedProperty
+        {
+            Name = "Direction",
+            Description = "Direction of an observation",
+            Definition = "Direction",
+        };
+    }
+
+    public static ObservedProperty Count()
+    {
+        return new ObservedProperty
+        {
+            Name = "Count",
+            Description = "Count of an observation",
+            Definition = "Count",
+        };
+    }
 }
